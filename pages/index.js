7e2bba12b8b7f24d292d7af0773c9e768dfcb9dc
Laurent Louf <laurent.louf@iot-makers.com>
2017-11-21 15:48:45 +0100

    Add a very basic heatmap to the project. Location of points is random and to simulate data, all points from the database are considered to come from different sensors.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import * as superagent from 'superagent';
 import 'isomorphic-fetch';
 import {LineChart} from 'react-d3';
 
+const ReactHeatmap = require('react-heatmap');
+
 export default class extends Component {
 
   static async getInitialProps ({ req }) 
@@ -46,15 +48,17 @@ export default class extends Component {
   {
     var deviceData = this.props.list ;
 
-    var lineData = [] ;
+    var lineData = [], heatData = [] ;
     for ( var iDevice = 0 ; iDevice < deviceData.length ; iDevice++ )
     {
       if ( deviceData[iDevice].data.length > 0 )
       {
         var line = {name: deviceData[iDevice].fullID, values: [] } ;
+
         for ( var iData = 0 ; iData < deviceData[iDevice].data.length ; iData++  )
         {
           line.values.push({x: new Date(deviceData[iDevice].data[iData].date), y: deviceData[iDevice].data[iData].data }) ; 
+          heatData.push({x: Math.random() * 100, y: Math.random() * 100, value: deviceData[iDevice].data[iData].data}) ; 
         }
         
         lineData.push(line) ; 
@@ -64,11 +68,17 @@ export default class extends Component {
 
     return (
         <div>
-          <LineChart
-            data={lineData}
-            width={1500}
-            height={800}
-          />
+          <div>
+            <LineChart
+              data={lineData}
+              width={1500}
+              height={800}
+            />
+          </div>
+
+          <div style={{width: 950, height: 850, backgroundImage: `url('https://lh3.googleusercontent.com/5kzwGd1E6wRFFhTQDAQZXj7LFi_epSer1DLXF4-UF3JlROMorWCf0QN2CgftQhcq_CSBt8hOWDwVuNAWeFqyZBKUSFMCYpxz_19IDamHH9ffi86_0bA19OLaXeTAXZcDHe_USj7IeGvamy7PW_5Lq51rEbY-a3luKkGGyLslr9eoDyAKfCG86wFgUzCZlWumo6wGGS0NYq__TrIC9uvkHb1X6fpB43rDjriTVttDPi8PJ4f9LBI4MLUDdslkr44KeK9dENB1CuVsKQzSEJ3LlRNvLBa97wpcLY5A6lqvpyZWtYKTfPFW-fgfWyrUi2s0RaV4OTZZ7Fn5G991sqX9lX0V2erCToJ29pgjXSsaF6qempSJ3EtW9lrWgJE3KOfbrl2qlDqZCh2YsdlYKx-r4cAfr4wrgg8bJwU1oCDyZ2mAHGelNDaU1mDNhZtVgdHqaqlOwMliG8N1UbMghCcWLiVIbEnAm9zSIsc9_HSNyZLxImkYX-MVNrUp1pW2fnfBP8oNcleFHkps7f2YqOGKh8oqh6BKM4CiTxtrNedc0tn8gahTr47NDAk2X8o7lj2SPmp9TYUn1gsvhIejaCB5MCVSOPc4deWDaAbKq7O3=w950-h850-no')`}} >
+            <ReactHeatmap max={20} data={heatData} radius={100} />
+          </div>
         </div>
     );
   }
