c7b1056acfd92d18e24ada0f29c039104276b924
Laurent Louf <laurent.louf@iot-makers.com>
2017-12-12 18:01:33 +0100

    Add a blink animation when the configuration was succesfully sent to the configuration cards.

--- public/js/config.js
+++ public/js/config.js
@@ -1,14 +1,42 @@
 $(function() 
 {
+	var timeouts = [] ; 
+
+	function blinkCard(id)
+	{
+		for ( var iTimeout = 0 ; iTimeout < timeouts.length ; iTimeout ++ )
+		{
+			if ( timeouts[iTimeout].id == id )
+				clearTimeout(timeouts[iTimeout].timeout ) ; 
+		}
+
+		timeouts.push({id: id, timeout: setTimeout( function() 
+		{
+			$( "#"+id ).queue(function() { $( this ).addClass( "border-success" ).dequeue(); }).delay( 100 ).queue(function() { $( this ).removeClass( "border-success" ).dequeue(); }).delay( 100 ).queue(function() { $( this ).addClass( "border-success" ).dequeue(); }).delay( 100 ).queue(function() { $( this ).removeClass( "border-success" ).dequeue(); }).delay( 100 ).queue(function() { $( this ).addClass( "border-success" ).dequeue(); }).delay( 100 ).queue(function() { $( this ).removeClass( "border-success" ).dequeue(); }).delay( 100 ) ;
+		}, 1500) } ) ; 
+	}
+
 	$( "select" ).change(function() 
 	{	
-		$.post( {url:"./", contentType : 'application/json', data: JSON.stringify({ fullID: $(this).attr("id").split("-")[1], field: $(this).attr("id").split("-")[0], value: $(this)[0].options[$(this)[0].selectedIndex].value }) } );
+		var splitID = $(this).attr("id").split("-") ; 
+		
+		$.post({
+			url:"./", 
+			contentType : 'application/json', 
+			data: JSON.stringify({ fullID: splitID[1], field: splitID[0], value: $(this)[0].options[$(this)[0].selectedIndex].value }),
+			success: function(response) { blinkCard(response.fullID) ; } }) ;
 	}) ; 
 
 
 	$( "input" ).change(function () 
 	{
-		$.post( {url:"./", contentType : 'application/json', data: JSON.stringify({ fullID: $(this).attr("id").split("-")[1], field: $(this).attr("id").split("-")[0], value: $(this).val() }) } );
+		var splitID = $(this).attr("id").split("-") ; 
+		
+		$.post( {
+			url:"./", 
+			contentType : 'application/json', 
+			data: JSON.stringify({ fullID: splitID[1], field: splitID[0], value: $(this).val() }),
+			success: function(response) { blinkCard(response.fullID) ; } }) ;
 	}) ;
 
 
